05671927c30c8066e6686bc3ab8c9950e9e87136
samayk27 <samaykumar2712@gmail.com>
2026-03-04 20:56:05 +0530

    response writers and parser changes

--- include/protocol.h
+++ include/protocol.h
@@ -1,17 +1,26 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
+#define RESP_MAX_ARGS 16
 
+#include <stdio.h>
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 typedef struct{
     int argc;
-    char *argv[16];
-    int arglen[16];
+    char *argv[RESP_MAX_ARGS];
+    size_t arglen[RESP_MAX_ARGS];
 }RESPcommand;
 
 int resp_parse(int fd, RESPcommand *cmd);
+
+ssize_t resp_write_simple_error(int fd,const char *msg, char resp_0);
+ssize_t resp_write_simple(int fd, const char *msg);
+ssize_t resp_write_error(int fd, const char *msg);
+ssize_t resp_write_bulk(int fd, const char *data, size_t len);
+ssize_t resp_write_null(int fd);
 
 #endif 
--- src/protocol.c
+++ src/protocol.c
@@ -36,18 +36,49 @@ int resp_parse(int fd, RESPcommand *cmd){
     if(type != '*') return -1;
     int argc;
     argc = read_number(fd);
+    if (argc < 1 || argc > RESP_MAX_ARGS) return -1;
     cmd->argc = argc;
     for(int i=0; i<argc; i++){
         if(read(fd, &type,1)!= 1) return -1;
         if(type != '$') return -1;
-        int arglen;
+
+        size_t arglen;
         arglen = read_number(fd);
         cmd->arglen[i] = arglen;
         cmd->argv[i] = malloc(arglen + 1);
+        if(cmd->argv[i] == NULL) return -1;
+
         cmd->argv[i][arglen] = '\0';
-        if(read(fd, cmd->argv[i], arglen) != arglen) return -1;
+        if(read(fd, cmd->argv[i], arglen) != (ssize_t)arglen) return -1;
         char crlf[2];
         read(fd, crlf, 2);
     }
     return 0;
+}
+
+ssize_t resp_write_simple_error(int fd,const char *msg, char resp_0){
+    char response[1+strlen(msg)+2 + 1]; /* +1 for resp_0, +2 for \r\n, +1 for null terminator */
+    response[0]=resp_0;
+    strcpy(&response[1], msg);
+    strcat(response, "\r\n");
+    return write(fd, response, strlen(response));
+}
+
+ssize_t resp_write_simple(int fd, const char *msg){
+    return resp_write_simple_error(fd,msg,'+');
+}
+
+ssize_t resp_write_error(int fd, const char *msg){
+    return resp_write_simple_error(fd,msg,'-');
+}
+
+ssize_t resp_write_bulk(int fd, const char *msg, size_t msg_len){
+    if(strlen(msg)!=msg_len) return -1;
+    char response[6+msg_len+2 + 1]; /* +1 for null terminator */
+    snprintf(response, sizeof(response), "$%zu\r\n%s\r\n", msg_len, msg);
+    return write(fd, response, strlen(response));
+}
+
+ssize_t resp_write_null(int fd){
+    return write(fd, "$-1\r\n", 5);
 }
